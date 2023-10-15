aa18486bd41b8c9c31b47155110cc2e0c681d61e
Bill Ainsworth <voidtraveller@hotmail.com>
2023-10-15 08:54:16 -0400

    Completed registration field validations

--- src/app/User/login-reg/login-reg.component.ts
+++ src/app/User/login-reg/login-reg.component.ts
@@ -3,8 +3,8 @@ import { Login } from '../../Models/login';
 import { Register } from '../../Models/register';
 import { jwtAuth } from '../../Models/jwtAuth';
 import { AuthenticationService } from '../../services/authentication.service';
-import { Validators } from "@angular/forms";
-import { FormGroup, FormControl } from "@angular/forms";
+import { Validators, ValidationErrors, ValidatorFn } from "@angular/forms";
+import { FormGroup, FormControl, AbstractControl } from "@angular/forms";
 
 @Component({
   selector: 'app-login-reg',
@@ -29,12 +29,36 @@ export class LoginRegComponent {
     });
   }
 
+  onSubmit() {
+    this.Register(this.registerDto);
+  }
+
   signUpForm = new FormGroup({
     Name: new FormControl("", [
       Validators.required,
       Validators.minLength(5)
+    ]),
+    Email: new FormControl("", [
+      Validators.required,
+      Validators.email
+    ]),
+    Password: new FormControl("", [
+      Validators.required,
+      Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}$")
+    ]),
+    PasswordRepeated: new FormControl("", [
+      Validators.required,
+      Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}$")
     ])
-  })
+  }, { validators: passwordMatchingValidator});
 
 
 }
+
+
+export const passwordMatchingValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('Password');
+  const confirmPassword = control.get('PasswordRepeated');
+
+  return password?.value === confirmPassword?.value ? null : { notmatched: true };
+};
